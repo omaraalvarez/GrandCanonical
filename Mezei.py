cadb7729eec3abc66b7fc938de2172d42be65cb3
Omar Alvarez <omar_4e2@hotmail.com>
2019-09-17 14:36:50 -0500

    v0.5. Close to working

--- Mezei.py
+++ Mezei.py
@@ -15,22 +15,25 @@ import numpy as np
     
 def Mezei(ChemPot, V, T, R_Cut = 3.0):
     """     AMOUNT OF STEPS     """
-    MC_Relaxation_Steps = 2000
-    MC_Equilibrium_Steps = 5000
+    MC_Relaxation_Steps = 5000
+    MC_Equilibrium_Steps = 25000
     MC_Steps = MC_Equilibrium_Steps + MC_Relaxation_Steps
-    MC_Measurement = 100
+    MC_Measurement = 300
 
     x, y, z = [], [], []
     L = m.pow(V, 1. / 3.)
     Beta = 1. / T
-    N_t, Pc, Pc_Sum, Pc_N = 500, dict(), dict(), dict()
+    Pc, Pc_Sum, Pc_N = dict(), dict(), dict()
     Overlap = 0.75
-    Displacement, N_Displacement, N_Accepted_Displacement = L / 10., 0, 0
+    Displacement, N_Displacement, N_Accepted_Displacement = L / 8, 0, 0
     N_Movement, N_Movement_Accepted, N_Movement_Rejected = 0, 0, 0
     N_Insertion, N_Insertion_Accepted, N_Insertion_Rejected = 0, 0, 0
     N_Removal, N_Removal_Accepted, N_Removal_Rejected = 0, 0, 0
     Energy, N_Measurements = 0., 0
     Energy_Sum, N_Sum = 0., 0.
+
+    Average_Density_File = open("Average_Density.dat", "w+")
+
     for i in range(MC_Steps):    
         """ PRINTS TO SCREEN SUMMARY """
         if i > 0 and i < MC_Relaxation_Steps and i % int(.01 * MC_Relaxation_Steps) == 0:
@@ -42,7 +45,7 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
             print("")
 
         if i > 0 and i > MC_Relaxation_Steps and i % int(0.01 * (MC_Steps - MC_Relaxation_Steps)) == 0:
-            print(100*(i - MC_Relaxation_Steps) / (MC_Equilibrium_Steps), "% Equilibrium")
+            print(int(100*(i - MC_Relaxation_Steps) / (MC_Equilibrium_Steps)), "% Equilibrium")
             print("U / N = ", Energy/len(x))
             print("N = ", len(x))
             print("Density = ", len(x)/V)
@@ -87,17 +90,16 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
                 N_Accepted_Displacement += 1
                 Energy += Delta_E
             else:
-                N_Movement_Rejected
+                N_Movement_Rejected += 1
                 x[j] = x_Old
                 y[j] = y_Old
                 z[j] = z_Old
 
         RN = rn.random()
-        control = len(x)
         if RN < 0.5:
             """            INSERTION          """
             N_Insertion += 1
-            grid = 32
+            grid = 8
             EVMPS = np.zeros((grid, grid, grid))
             Delta = L / (grid + 1)
             for i_x in range(0, grid, 1):
@@ -111,7 +113,7 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
                             Delta_y = y_grid - y[k]
                             Delta_z = z_grid - z[k]
                             r2 = m.pow(Delta_x, 2) + m.pow(Delta_y, 2) + m.pow(Delta_z, 2)
-                            if r2 < m.pow(0.8, 2):
+                            if r2 < m.pow(Overlap, 2):
                                 EVMPS[i_x, i_y, i_z] = 1
                                 break
             if np.sum(EVMPS) < m.pow(grid, 3):
@@ -125,29 +127,29 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
                 z_Insertion = (i_z + 1) * Delta - L / 2.
                 Energy_Insertion = Energy_Virial(L, R_Cut, x_Insertion, y_Insertion, z_Insertion, x, y, z)
                 """     PROBABILITY Pc      """
-                N_i = N_t
-                for j in range(N_t):
-                    rx = L*(rn.random() - 0.5)
-                    ry = L*(rn.random() - 0.5)
-                    rz = L*(rn.random() - 0.5)
-                    for k in range(len(x)):
-                        Delta_x = rx - x[k]
-                        Delta_y = ry - y[k]
-                        Delta_z = rz - z[k]
-                        r2 = m.pow(Delta_x, 2) + m.pow(Delta_y, 2) + m.pow(Delta_z, 2)
-                        if r2 < m.pow(Overlap, 2):
-                            N_i -= 1
+                #N_i = N_t
+                #for j in range(N_t):
+                #    rx = L*(rn.random() - 0.5)
+                #    ry = L*(rn.random() - 0.5)
+                #    rz = L*(rn.random() - 0.5)
+                #    for k in range(len(x)):
+                #        Delta_x = rx - x[k]
+                #        Delta_y = ry - y[k]
+                #        Delta_z = rz - z[k]
+                #        r2 = m.pow(Delta_x, 2) + m.pow(Delta_y, 2) + m.pow(Delta_z, 2)
+                #        if r2 < m.pow(Overlap, 2):
+                #            N_i -= 1
                             #if N_i < 1:
                             #    raise ValueError("Ni cant be negative")
-                            break
+                #            break
 
                 if len(x) not in Pc_N:
                     Pc_N[len(x)] = 1
-                    Pc_Sum[len(x)] = N_i/N_t
-                    Pc[len(x)] = N_i/N_t
+                    Pc_Sum[len(x)] = 1 - (np.sum(EVMPS) / m.pow(grid, 3)) #N_i/N_t
+                    Pc[len(x)] = 1 - (np.sum(EVMPS) / m.pow(grid, 3)) #N_i/N_t
                 else:
                     Pc_N[len(x)] += 1
-                    Pc_Sum[len(x)] += N_i/N_t
+                    Pc_Sum[len(x)] += 1 - (np.sum(EVMPS) / m.pow(grid, 3)) #N_i/N_t
                     Pc[len(x)] = Pc_Sum[len(x)] / Pc_N[len(x)]
                 if rn.random() < (V * Pc[len(x)] / (len(x) + 1)) * m.exp(Beta * (ChemPot - Energy_Insertion)):
                     N_Insertion_Accepted += 1
@@ -223,13 +225,13 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
 
                     lim_inf = len(x) - 1
                     while True:
-                        if lim_inf in PC:
+                        if lim_inf in Pc:
                             break
                         lim_inf -= 1
 
                     lim_sup = len(x) + 1
                     while True:
-                        if lim_sup in PC:
+                        if lim_sup in Pc:
                             break
                         lim_sup += 1
                 elif len(Pc) == 0:
@@ -239,7 +241,7 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
             else:
                 Pc_Interpolation = Pc[len(x) - 1]
 
-            if rn.random() > m.pow(1 - m.pow(Pc_Interpolation, len(x) - 1), N_t):
+            if rn.random() > m.pow(1 - m.pow(Pc_Interpolation, len(x) - 1), m.pow(grid, 3)):
                 Energy_Removal = Energy_Virial(L, R_Cut, x[j], y[j], z[j], x, y, z)
                 if rn.random() < (len(x) / (V * Pc_Interpolation)) * m.exp(Beta * (Energy_Removal - ChemPot)):
                     N_Removal_Accepted += 1
@@ -262,10 +264,12 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
 
         if i > 0:
             if m.fmod(i, MC_Measurement) == 0:
-                """     COMPUTES AVERAGES    """
-                N_Measurements += 1
-                Energy_Sum += Energy/len(x)
-                N_Sum += len(x)
+                if i >= MC_Relaxation_Steps:
+                    """     COMPUTES AVERAGES    """
+                    N_Measurements += 1
+                    Energy_Sum += Energy/len(x)
+                    N_Sum += len(x)
+                    Average_Density_File.write("%d\t%f\t%f\n" % (N_Measurements, (N_Sum/N_Measurements), (N_Sum/(N_Measurements * V))))
 
                 """ PARTICLE DISPLACEMENT """
                 if 1. * N_Accepted_Displacement / N_Displacement > 0.55:
@@ -279,6 +283,12 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
 
                 N_Accepted_Displacement, N_Displacement = 0, 0
     
+    Average_Density_File.close()
+    Pc_File = open("Pc.dat", "w+")
+    for i in Pc:
+        Pc_File.write("%d\t%f\n" % (i, Pc[i]))
+    Pc_File.close()
+
     print("< E / N > = ", Energy_Sum / N_Measurements)
     print("< Density > = ", N_Sum / (N_Measurements * V))
     print("< N > = ", N_Sum/N_Measurements)
@@ -325,4 +335,4 @@ def Energy_Virial(L, R_Cut, rx, ry, rz, x, y, z):
                 Energy += u(r2)
     return Energy
 
-Mezei(-0.0430757, 249.68789, 2.0)
+Mezei(17.918385914, 250.05869922517022, 4.0)
