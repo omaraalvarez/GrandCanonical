fe1dbb1a570f9b6fb90419929f19077b748b3338
Omar Alvarez <omar_4e2@hotmail.com>
2019-09-17 17:30:15 -0500

    Adding output files

--- Mezei.py
+++ Mezei.py
@@ -15,25 +15,30 @@ import numpy as np
     
 def Mezei(ChemPot, V, T, R_Cut = 3.0):
     """     AMOUNT OF STEPS     """
-    MC_Relaxation_Steps = 5000
-    MC_Equilibrium_Steps = 25000
+    MC_Relaxation_Steps = 20000
+    MC_Equilibrium_Steps = 10000
     MC_Steps = MC_Equilibrium_Steps + MC_Relaxation_Steps
-    MC_Measurement = 300
+    MC_Measurement = 100
 
     x, y, z = [], [], []
     L = m.pow(V, 1. / 3.)
     Beta = 1. / T
-    Pc, Pc_Sum, Pc_N = dict(), dict(), dict()
     Overlap = 0.75
+    Pc, Pc_Sum, Pc_N = dict(), dict(), dict()
     Displacement, N_Displacement, N_Accepted_Displacement = L / 8, 0, 0
     N_Movement, N_Movement_Accepted, N_Movement_Rejected = 0, 0, 0
     N_Insertion, N_Insertion_Accepted, N_Insertion_Rejected = 0, 0, 0
     N_Removal, N_Removal_Accepted, N_Removal_Rejected = 0, 0, 0
-    Energy, N_Measurements = 0., 0
-    Energy_Sum, N_Sum = 0., 0.
+    Energy, Virial, N_Measurements = 0., 0., 0
+    Energy_Sum, Virial_Sum, N_Sum = 0., 0., 0
 
+    Average_Energy_File = open("Average_Energy.dat", "w+")
+    Average_Energy_File.write("#\t< E / N >\n")
+    Average_Pressure_File = open("Average_Pressure.dat", "w+")
+    Average_Pressure_File.write("#\t< P >\n")
     Average_Density_File = open("Average_Density.dat", "w+")
-
+    Average_Density_File.write("#\t< Density >\t< N >\n")
+    
     for i in range(MC_Steps):    
         """ PRINTS TO SCREEN SUMMARY """
         if i > 0 and i < MC_Relaxation_Steps and i % int(.01 * MC_Relaxation_Steps) == 0:
@@ -61,7 +66,7 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
             N_Movement += 1
             N_Displacement += 1
             j = rn.randrange(0, len(x), 1)
-            Energy_Old = Energy_Virial(L, R_Cut, x[j], y[j], z[j], x, y, z)
+            Energy_Old, Virial_Old = Energy_Virial(L, R_Cut, x[j], y[j], z[j], x, y, z)
             x_Old, y_Old, z_Old = x[j], y[j], z[j]
 
             """     DISPLACEMENT AND PERIODIC BOUNDARY CONDITIONS """
@@ -83,12 +88,14 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
             elif z[j] > L / 2.:
                 z[j] -= L
             
-            Energy_New = Energy_Virial(L, R_Cut, x[j], y[j], z[j], x, y, z)
+            Energy_New, Virial_New = Energy_Virial(L, R_Cut, x[j], y[j], z[j], x, y, z)
             Delta_E = Energy_New - Energy_Old
+            Delta_Virial = Virial_New - Virial_Old
             if rn.random() < m.exp(-Beta * Delta_E):
                 N_Movement_Accepted += 1
                 N_Accepted_Displacement += 1
                 Energy += Delta_E
+                Virial += Delta_Virial
             else:
                 N_Movement_Rejected += 1
                 x[j] = x_Old
@@ -125,7 +132,7 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
                 x_Insertion = (i_x + 1) * Delta - L / 2.
                 y_Insertion = (i_y + 1) * Delta - L / 2.
                 z_Insertion = (i_z + 1) * Delta - L / 2.
-                Energy_Insertion = Energy_Virial(L, R_Cut, x_Insertion, y_Insertion, z_Insertion, x, y, z)
+                Energy_Insertion, Virial_Insertion = Energy_Virial(L, R_Cut, x_Insertion, y_Insertion, z_Insertion, x, y, z)
                 """     PROBABILITY Pc      """
                 #N_i = N_t
                 #for j in range(N_t):
@@ -157,6 +164,7 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
                     y.append(y_Insertion)
                     z.append(z_Insertion)
                     Energy += Energy_Insertion
+                    Virial += Virial_Insertion
                 else:
                     N_Insertion_Rejected += 1
 
@@ -201,13 +209,14 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
                 x_Insertion = L*(rn.random() - 0.5)
                 y_Insertion = L*(rn.random() - 0.5)
                 z_Insertion = L*(rn.random() - 0.5)
-                Energy_Insertion = Energy_Virial(L, R_Cut, x_Insertion, y_Insertion, z_Insertion, x, y, z)
+                Energy_Insertion, Virial_Insertion = Energy_Virial(L, R_Cut, x_Insertion, y_Insertion, z_Insertion, x, y, z)
                 if rn.random() < m.exp(Beta * (ChemPot - Energy_Insertion) + m.log(V / (len(x) + 1))):
                     N_Insertion_Accepted += 1
                     x.append(x_Insertion)
                     y.append(y_Insertion)
                     z.append(z_Insertion)
                     Energy += Energy_Insertion
+                    Virial += Virial_Insertion
                 else:
                     N_Insertion_Rejected += 1
 
@@ -216,8 +225,8 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
             N_Removal += 1
             j = rn.randrange(0, len(x), 1)
 
-            """   Actual interpolacion va de N -> N - 1 y N + 1
-                    Interpolacion sugerida por Mezei va de N-1 -> N y N + 1     """
+            """   IN CASE THE VALUE REQUIRED DOES'T EXIST
+                    A LINEAR INTERPOLATION IS CARRIED ON.  """
             if (len(x) - 1) not in Pc:
                 if len(Pc) == 1:
                     Pc_Interpolation =  Pc[ list( Pc.keys() )[0] ]
@@ -242,23 +251,25 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
                 Pc_Interpolation = Pc[len(x) - 1]
 
             if rn.random() > m.pow(1 - m.pow(Pc_Interpolation, len(x) - 1), m.pow(grid, 3)):
-                Energy_Removal = Energy_Virial(L, R_Cut, x[j], y[j], z[j], x, y, z)
+                Energy_Removal, Virial_Removal = Energy_Virial(L, R_Cut, x[j], y[j], z[j], x, y, z)
                 if rn.random() < (len(x) / (V * Pc_Interpolation)) * m.exp(Beta * (Energy_Removal - ChemPot)):
                     N_Removal_Accepted += 1
                     x.pop(j)
                     y.pop(j)
                     z.pop(j)
                     Energy -= Energy_Removal
+                    Virial -= Virial_Removal
                 else:
                     N_Removal_Rejected += 1
             else: 
-                Energy_Removal = Energy_Virial(L, R_Cut, x[j], y[j], z[j], x, y, z)
+                Energy_Removal, Virial_Removal = Energy_Virial(L, R_Cut, x[j], y[j], z[j], x, y, z)
                 if rn.random() < m.exp(Beta * (Energy_Removal - ChemPot) + m.log(len(x) / V)):
                     N_Removal_Accepted += 1
                     x.pop(j)
                     y.pop(j)
                     z.pop(j)
                     Energy -= Energy_Removal
+                    Virial -= Virial_Removal
                 else:
                     N_Removal_Rejected += 1
 
@@ -268,8 +279,11 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
                     """     COMPUTES AVERAGES    """
                     N_Measurements += 1
                     Energy_Sum += Energy/len(x)
+                    Average_Energy_File.write("%d\t%f\n" % (N_Measurements, Energy_Sum/N_Measurements))
+                    Virial_Sum += Virial
+                    Average_Pressure_File.write("%d\t%f\n" % (N_Measurements, (N_Sum * T  - Virial / 3.0) / (N_Measurements * V) ))
                     N_Sum += len(x)
-                    Average_Density_File.write("%d\t%f\t%f\n" % (N_Measurements, (N_Sum/N_Measurements), (N_Sum/(N_Measurements * V))))
+                    Average_Density_File.write("%d\t%f\t%f\n" % (N_Measurements, (N_Sum/(N_Measurements * V)), (N_Sum/N_Measurements) ))
 
                 """ PARTICLE DISPLACEMENT """
                 if 1. * N_Accepted_Displacement / N_Displacement > 0.55:
@@ -283,13 +297,17 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
 
                 N_Accepted_Displacement, N_Displacement = 0, 0
     
+    Average_Energy_File.close()
+    Average_Pressure_File.close()
     Average_Density_File.close()
+
     Pc_File = open("Pc.dat", "w+")
     for i in Pc:
         Pc_File.write("%d\t%f\n" % (i, Pc[i]))
     Pc_File.close()
 
     print("< E / N > = ", Energy_Sum / N_Measurements)
+    print("< P > = ", (N_Sum * T  - Virial / 3.0) / (N_Measurements * V) )
     print("< Density > = ", N_Sum / (N_Measurements * V))
     print("< N > = ", N_Sum/N_Measurements)
     print(" ")
@@ -307,9 +325,11 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
 def u(r2):
     return 4.0*(m.pow(1. / r2, 6.0) - m.pow(1. / r2, 3.))
 
+def rdu(r2):
+    return 4.0*(6*m.pow(1. / r2, 3.0) - 12.0 * m.pow(1.0 / r2, 6.0))
 
 def Energy_Virial(L, R_Cut, rx, ry, rz, x, y, z):
-    Energy = 0.
+    Energy, Virial = 0., 0.
     for i in range(len(x)):
         Delta_x = rx - x[i]
         Delta_y = ry - y[i]
@@ -333,6 +353,7 @@ def Energy_Virial(L, R_Cut, rx, ry, rz, x, y, z):
         if r2 != 0.0:
             if r2 < m.pow(R_Cut, 2):
                 Energy += u(r2)
-    return Energy
+                Virial += rdu(r2)
+    return Energy, Virial
 
-Mezei(17.918385914, 250.05869922517022, 4.0)
+Mezei(17.9183859, 250.0586992251, 4.0)    
