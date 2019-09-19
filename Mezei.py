31b909f21efc8903341edf617a153682b599509d
Omar Alvarez <omar_4e2@hotmail.com>
2019-09-19 01:30:12 -0500

    Fixed output route for files

--- Mezei.py
+++ Mezei.py
@@ -9,15 +9,20 @@
 #                                                            #
 ##############################################################
 
-import random as rn
 import math as m
 import numpy as np
-import statistics as stat
-    
+import os
+
+#from numba import jit
+from random import random, randrange
+from statistics import mean, pstdev
+from timeit import default_timer as timer
+           
 def Mezei(ChemPot, V, T, R_Cut = 3.0):
+    start = timer()
     """     AMOUNT OF STEPS     """
-    MC_Relaxation_Steps = 20000
-    MC_Equilibrium_Steps = 10000
+    MC_Relaxation_Steps = 1000
+    MC_Equilibrium_Steps = 1000
     MC_Steps = MC_Equilibrium_Steps + MC_Relaxation_Steps
     MC_Measurement = 100
 
@@ -33,11 +38,14 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
     Energy, Virial = 0., 0.
     Energy_Array, Pressure_Array, Density_Array = [], [], []
 
-    Average_Energy_File = open("Average_Energy.dat", "w+")
+    Output_Route = "Output/ChPot_%.3f_T_%.2f" % (ChemPot, T)
+    if not os.path.exists(Output_Route):
+        os.makedirs(Output_Route)
+    Average_Energy_File = open("%s/Average_Energy.dat" % Output_Route, "w+")
     Average_Energy_File.write("#\t< E / N >\n")
-    Average_Pressure_File = open("Average_Pressure.dat", "w+")
+    Average_Pressure_File = open("%s/Average_Pressure.dat" % Output_Route, "w+")
     Average_Pressure_File.write("#\t< P >\n")
-    Average_Density_File = open("Average_Density.dat", "w+")
+    Average_Density_File = open("%s/Average_Density.dat" % Output_Route, "w+")
     Average_Density_File.write("#\t< Density >\n")
     
     for i in range(MC_Steps):    
@@ -68,24 +76,24 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
             """            MOVEMENT           """
             N_Movement += 1
             N_Displacement += 1
-            j = rn.randrange(0, len(x), 1)
+            j = randrange(0, len(x), 1)
             Energy_Old, Virial_Old = Energy_Virial(L, R_Cut, x[j], y[j], z[j], x, y, z)
             x_Old, y_Old, z_Old = x[j], y[j], z[j]
 
             """     DISPLACEMENT AND PERIODIC BOUNDARY CONDITIONS """
-            x[j] += Displacement * (rn.random() - 0.5)
+            x[j] += Displacement * (random() - 0.5)
             if x[j] <  -L / 2.:
                 x[j] += L
             elif x[j] > L / 2.:
                 x[j] -= L
                 
-            y[j] += Displacement * (rn.random() - 0.5)
+            y[j] += Displacement * (random() - 0.5)
             if y[j] <  -L / 2.:
                 y[j] += L
             elif y[j] > L / 2.:
                 y[j] -= L
 
-            z[j] += Displacement * (rn.random() - 0.5)
+            z[j] += Displacement * (random() - 0.5)
             if z[j] <  -L / 2.:
                 z[j] += L
             elif z[j] > L / 2.:
@@ -94,7 +102,7 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
             Energy_New, Virial_New = Energy_Virial(L, R_Cut, x[j], y[j], z[j], x, y, z)
             Delta_E = Energy_New - Energy_Old
             Delta_Virial = Virial_New - Virial_Old
-            if rn.random() < m.exp(-Beta * Delta_E):
+            if random() < m.exp(-Beta * Delta_E):
                 N_Movement_Accepted += 1
                 N_Accepted_Displacement += 1
                 Energy += Delta_E
@@ -105,7 +113,7 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
                 y[j] = y_Old
                 z[j] = z_Old
 
-        RN = rn.random()
+        RN = random()
         if RN < 0.5:
             """            INSERTION          """
             N_Insertion += 1
@@ -128,7 +136,7 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
                                 break
             if np.sum(EVMPS) < m.pow(grid, 3):
                 aux1 = np.where(EVMPS == 0)
-                j = rn.randrange(0, len(aux1[0]), 1)
+                j = randrange(0, len(aux1[0]), 1)
                 i_x = aux1[0][j]
                 i_y = aux1[1][j]
                 i_z = aux1[2][j]
@@ -139,9 +147,9 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
                 """     PROBABILITY Pc      """
                 #N_i = N_t
                 #for j in range(N_t):
-                #    rx = L*(rn.random() - 0.5)
-                #    ry = L*(rn.random() - 0.5)
-                #    rz = L*(rn.random() - 0.5)
+                #    rx = L*(random() - 0.5)
+                #    ry = L*(random() - 0.5)
+                #    rz = L*(random() - 0.5)
                 #    for k in range(len(x)):
                 #        Delta_x = rx - x[k]
                 #        Delta_y = ry - y[k]
@@ -161,7 +169,7 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
                     Pc_N[len(x)] += 1
                     Pc_Sum[len(x)] += 1 - (np.sum(EVMPS) / m.pow(grid, 3)) #N_i/N_t
                     Pc[len(x)] = Pc_Sum[len(x)] / Pc_N[len(x)]
-                if rn.random() < (V * Pc[len(x)] / (len(x) + 1)) * m.exp(Beta * (ChemPot - Energy_Insertion)):
+                if random() < (V * Pc[len(x)] / (len(x) + 1)) * m.exp(Beta * (ChemPot - Energy_Insertion)):
                     N_Insertion_Accepted += 1
                     x.append(x_Insertion)
                     y.append(y_Insertion)
@@ -199,7 +207,7 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
                 #Volume_Ratio = (V_Excluded - V_Excluded_Correction) / V
                 #if Volume_Ratio > 1 or Volume_Ratio < 0:
                 #    raise ValueError("Volume Ratio (", Volume_Ratio, ") can't be negative nor greater than one.")
-                #if rn.random() < (V_Excluded - V_Excluded_Correction)/(len(x) + 1) * m.exp(Beta * (ChemPot - Energy_Insertion))
+                #if random() < (V_Excluded - V_Excluded_Correction)/(len(x) + 1) * m.exp(Beta * (ChemPot - Energy_Insertion))
                 #    N_Insertion_Accepted += 1
                 #    x.append(x_Insertion)
                 #    y.append(y_Insertion)
@@ -209,11 +217,11 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
                 #    N_Insertion_Rejected += 1
 
             else:        
-                x_Insertion = L*(rn.random() - 0.5)
-                y_Insertion = L*(rn.random() - 0.5)
-                z_Insertion = L*(rn.random() - 0.5)
+                x_Insertion = L*(random() - 0.5)
+                y_Insertion = L*(random() - 0.5)
+                z_Insertion = L*(random() - 0.5)
                 Energy_Insertion, Virial_Insertion = Energy_Virial(L, R_Cut, x_Insertion, y_Insertion, z_Insertion, x, y, z)
-                if rn.random() < m.exp(Beta * (ChemPot - Energy_Insertion) + m.log(V / (len(x) + 1))):
+                if random() < m.exp(Beta * (ChemPot - Energy_Insertion) + m.log(V / (len(x) + 1))):
                     N_Insertion_Accepted += 1
                     x.append(x_Insertion)
                     y.append(y_Insertion)
@@ -226,7 +234,7 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
         elif len(x) > 1:
             """            REMOVAL            """
             N_Removal += 1
-            j = rn.randrange(0, len(x), 1)
+            j = randrange(0, len(x), 1)
 
             """   IN CASE THE VALUE REQUIRED DOES'T EXIST
                     A LINEAR INTERPOLATION IS CARRIED ON.  """
@@ -253,9 +261,9 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
             else:
                 Pc_Interpolation = Pc[len(x) - 1]
 
-            if rn.random() > m.pow(1 - m.pow(Pc_Interpolation, len(x) - 1), m.pow(grid, 3)):
+            if random() > m.pow(1 - m.pow(Pc_Interpolation, len(x) - 1), m.pow(grid, 3)):
                 Energy_Removal, Virial_Removal = Energy_Virial(L, R_Cut, x[j], y[j], z[j], x, y, z)
-                if rn.random() < (len(x) / (V * Pc_Interpolation)) * m.exp(Beta * (Energy_Removal - ChemPot)):
+                if random() < (len(x) / (V * Pc_Interpolation)) * m.exp(Beta * (Energy_Removal - ChemPot)):
                     N_Removal_Accepted += 1
                     x.pop(j)
                     y.pop(j)
@@ -266,7 +274,7 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
                     N_Removal_Rejected += 1
             else: 
                 Energy_Removal, Virial_Removal = Energy_Virial(L, R_Cut, x[j], y[j], z[j], x, y, z)
-                if rn.random() < m.exp(Beta * (Energy_Removal - ChemPot) + m.log(len(x) / V)):
+                if random() < m.exp(Beta * (Energy_Removal - ChemPot) + m.log(len(x) / V)):
                     N_Removal_Accepted += 1
                     x.pop(j)
                     y.pop(j)
@@ -281,11 +289,11 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
                 if i >= MC_Relaxation_Steps:
                     """     COMPUTES AVERAGES    """
                     Energy_Array.append( Energy/len(x) )
-                    Average_Energy_File.write("%d\t%f\n" % (len(Energy_Array), stat.mean(Energy_Array)))
+                    Average_Energy_File.write("%d\t%f\n" % (len(Energy_Array), mean(Energy_Array)))
                     Pressure_Array.append( (len(x) * T - Virial / 3.0) / V )
-                    Average_Pressure_File.write("%d\t%f\n" % (len(Pressure_Array), stat.mean(Pressure_Array)))
+                    Average_Pressure_File.write("%d\t%f\n" % (len(Pressure_Array), mean(Pressure_Array)))
                     Density_Array.append( len(x) / V ) 
-                    Average_Density_File.write("%d\t%f\n" % (len(Density_Array), stat.mean(Density_Array) ))
+                    Average_Density_File.write("%d\t%f\n" % (len(Density_Array), mean(Density_Array) ))
 
                 """ PARTICLE DISPLACEMENT """
                 if 1. * N_Accepted_Displacement / N_Displacement > 0.55:
@@ -303,15 +311,15 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
     Average_Pressure_File.close()
     Average_Density_File.close()
 
-    Pc_File = open("Pc.dat", "w+")
+    Pc_File = open("%s/Pc.dat" % Output_Route, "w+")
     for i in Pc:
         Pc_File.write("%d\t%f\n" % (i, Pc[i]))
     Pc_File.close()
 
-    print("< E / N > = %.6f + %.6f" % (stat.mean(Energy_Array), stat.pstdev(Energy_Array)) )
-    print("< P > = %.6f + %.6f" % (stat.mean(Pressure_Array), stat.pstdev(Pressure_Array)) )
-    print("< Density > = %.6f + %.6f " % (stat.mean(Density_Array), stat.pstdev(Density_Array)) )
-    print("< N > = %.6f + %.6f " % (V*stat.mean(Density_Array), V*stat.pstdev(Density_Array)) )
+    print("< E / N > = %.6f + %.6f" % (mean(Energy_Array), pstdev(Energy_Array)) )
+    print("< P > = %.6f + %.6f" % (mean(Pressure_Array), pstdev(Pressure_Array)) )
+    print("< Density > = %.6f + %.6f " % (mean(Density_Array), pstdev(Density_Array)) )
+    print("< N > = %.6f + %.6f " % (V*mean(Density_Array), V*pstdev(Density_Array)) )
     print(" ")
     print("Movements: %d" % N_Movement)
     print("     Accepted: %d" % N_Movement_Accepted)
@@ -322,8 +330,10 @@ def Mezei(ChemPot, V, T, R_Cut = 3.0):
     print("Removal: %d" % N_Removal)
     print("     Accepted: %d" % N_Removal_Accepted)
     print("     Rejected: %d" % N_Removal_Rejected)
-                
 
+    dt = timer() - start
+    print("Elapsed time: %f s" % dt)
+          
 def u(r2):
     return 4.0*(m.pow(1. / r2, 6.0) - m.pow(1. / r2, 3.))
 
@@ -358,4 +368,4 @@ def Energy_Virial(L, R_Cut, rx, ry, rz, x, y, z):
                 Virial += rdu(r2)
     return Energy, Virial
 
-Mezei(17.9183859, 250.0586992251, 4.0)
+Mezei(-17.9183859, 250.0586992251, 4.0)
